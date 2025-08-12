125dd9a220ce0cc39ead26bd0cf2ca6c6d4704dc
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2025-08-12 15:25:53 -0700

    anonymizing

---
+++ cluster/parameter_models/adult_occupancy/inputs/convergence_check.R
@@ -0,0 +1,34 @@
+
+# Load packages ---------------------------------------------------------------
+
+# Load packages, here and tidyverse for coding ease, 
+package.list <- c("here", "dplyr",
+                  "tidyr", "ggplot2", 
+                  'mcmcplots', "stringr",
+                  "coda", "htmltools") #mcmc output
+
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% 
+                                 installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+
+# Import model ------------------------------------------------------------
+
+mod <- readRDS(file = "/scratch/user/whwo_ipm/parameter_models/adult_occupancy/outputs/adult_occupancy_inits_JAGS.RDS")
+
+
+# Check convergence -------------------------------------------------------
+
+mcmcplot(mod$samples,
+         dir = '/scratch/user/whwo_ipm/parameter_models/adult_occupancy/outputs/mcmcplots/initial')
+
+# Get RHat per parameter ------------------------------------------------
+
+Rhat <- mod$Rhat
+
+saveRDS(Rhat, '/scratch/user/whwo_ipm/parameter_models/adult_occupancy/outputs/model_init_Rhat.RDS')
